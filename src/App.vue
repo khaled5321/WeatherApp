723ce8ccf9daf1ae8fe3eaa797ee94d040cad0d0
Khaled Emad <khaledemad656@gmail.com>
2022-10-10 17:33:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Navbar/>
+  <RouterView/>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import Navbar from './components/navbar.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="bg-[#607253] shadow-md">
+        <div class="container mx-auto p-5 flex justify-between">
+            <RouterLink :to="{name:'home'}">
+                <div class="flex items-center gap-2 text-3xl">
+                    <i class="fa-solid fa-sun text-white"></i>
+                    <h1 class="text-[#d5f0c5]">Vweather</h1>
+                </div>
+            </RouterLink>
+            
+            <i class="fa-solid fa-circle-info text-3xl text-white
+            hover:cursor-pointer transition-colors hover:text-[#a3a3a3] duration-300"></i>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
