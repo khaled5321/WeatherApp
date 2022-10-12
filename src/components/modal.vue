3d9e32e6f533e869c08de7b3d2283148392fac55
Khaled Emad <khaledemad656@gmail.com>
2022-10-12 22:04:52 +0200

    added modal

---
+++ src/components/modal.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-if="true"
+    class="absolute flex items-center w-full top-0 left-0 h-full bg-black bg-opacity-30">
+        <div v-show="true"
+        class="flex flex-col w-[70vw] h-fit mx-auto bg-white rounded-md p-5 shadow-2xl">
+            
+            <div class="flex flex-col gap-2">
+                <h1 class="text-2xl">About:</h1>
+                <p class="mb-4">
+                    Vweather app allows you to track the current and
+                    future weather of cities of your choosing.
+                </p>
+            </div>
+
+            <div class="flex flex-col gap-2">
+                <h2 class="text-2xl">How it works:</h2>
+                <ol class="list-decimal list-inside mb-4 flex flex-col gap-2">
+                    <li>
+                        Search for your city by entering the name into the
+                        search bar.
+                    </li>
+                    <li>
+                        Select a city within the results, this will take
+                        you to the current weather for your selection.
+                    </li>
+                    <li>
+                        Track the city by clicking on the "+" icon in the
+                        top right. This will save the city to view at a
+                        later time on the home page.
+                    </li>
+                </ol>
+            </div>
+
+            <div class="flex flex-col gap-2">
+                <h2 class="text-2xl">Removing a city:</h2>
+                <p>
+                    If you no longer wish to track a city, simply select
+                    the city within the home page. At the bottom of the
+                    page, there will be an option to delete the city.
+                </p>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="bg-[#607253] shadow-md">
-        <div class="container mx-auto p-5 flex justify-between">
+        <nav class="container mx-auto p-5 flex justify-between">
             <RouterLink :to="{name:'home'}">
                 <div class="flex items-center gap-2 text-3xl">
                     <i class="fa-solid fa-sun text-white"></i>
@@ -10,10 +10,16 @@
             
             <i class="fa-solid fa-circle-info text-3xl text-white
             hover:cursor-pointer transition-colors hover:text-[#a3a3a3] duration-300"></i>
-        </div>
+        </nav>
+        
+        <Teleport to="body">
+            <Modal/>
+        </Teleport>
+
     </header>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router';
+import Modal from './modal.vue';
 </script>
